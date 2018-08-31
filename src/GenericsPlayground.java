7fc5b224cd40d6790780ec43c306b0312956d9db
Peng Zhang <irvingfish@gmail.com>
2018-08-31 16:42:09 +0200

    add more stuff

--- src/GenericsPlayground.java
+++ src/GenericsPlayground.java
@@ -8,7 +8,6 @@ public class GenericsPlayground {
         genericsPlayground.test();
     }
 
-
     interface Key<T> {
     }
 
@@ -26,10 +25,28 @@ public class GenericsPlayground {
             return map.get(key);
         }
 
+        Value<?> getty(Key<? extends B> key) {
+            return map.get(key);
+        }
+
+        //Generic method
         <T extends B> void modify(Map<Key<T>, Value<T>> map) {
         }
 
-        <T> void moooooodify(Map<Key<T>, Value<T>> map) {
+        //Generic method
+        <B> void moooooodify(Map<Key<B>, Value<B>> map) {
+        }
+
+        //Generic method
+        <T> void modifyyyyyy(Map<Key<T>, Value<T>> map) {
+        }
+
+        //member method
+         void modiiiify(Map<Key<B>, Value<B>> map) {
+        }
+
+        //Generic method
+        <T> void mmmmmmmodify(Map<Key<B>, Value<B>> map) {
         }
     }
 
@@ -41,16 +58,32 @@ public class GenericsPlayground {
 
         Value<? extends Number> value1 = myTypeMap.get(numberKey);
         Value<?> value2 = myTypeMap.get(numberKey);
-        assert (value1 == value2);
+        assert (value1 == value2);//add -ea in vm argument
 
         //cannot compile
         //Value<Object> value3 = myTypeMap.get(numberKey);
+        //Value<Long> value3 = myTypeMap.get(numberKey);
+        //Value<Number> value3 = myTypeMap.get(numberKey);
+
+        Value<?> value4 = myTypeMap.getty(numberKey);
+
+        //cannot compile
+        //Value<? extends Number> value5 = myTypeMap.getty(numberKey);
+        //Value<Long> value6 = myTypeMap.getty(numberKey);
 
 
         Map<Key<String>, Value<String>> keyValueMap = new HashMap<>();
+        myTypeMap.modifyyyyyy(keyValueMap);
+        myTypeMap.moooooodify(keyValueMap);
         //cannot compile
         //myTypeMap.modify(keyValueMap);
 
-        myTypeMap.moooooodify(keyValueMap);
+
+        Map<Key<Integer>, Value<Integer>> integerMap = new HashMap<>();
+        myTypeMap.modify(integerMap);
+
+        //cannot compile
+        //myTypeMap.modiiiify(integerMap);
+        //myTypeMap.mmmmmmmodify(integerMap);
     }
 }
